77395ba0fc12cd7d2d0b00d4631d814d746c6a0c
rob_mill <roberta.milasauskaite@gmail.com>
2020-05-23 19:05:37 +0300

    Add multiline (;) support

--- shell.py
+++ shell.py
@@ -2,7 +2,12 @@ import LiT
 
 while True:
     text = input()
+    if text.strip() == "": continue
     result, error = LiT.run('input file name', text)
 
     if error: print(error.toString())
-    elif result: print(repr(result))
+    elif result:
+       if len(result.elements) == 1:
+        print(repr(result.elements[0]))
+       else:
+        print(repr(result))
